60d16812cebfdb5ad2640738579a564e9db65b11
Victoria Kempter <victoria@thekempters.de>
2019-02-28 17:02:21 +0100

    now to the fun part

---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates :description, presence: true
+  validates_uniqueness_of :cocktail, scope: :ingredient
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Ingredients
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "ginger")
+Ingredient.create(name: "ginger beer")
+Ingredient.create(name: "cucumber")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "tonic")
